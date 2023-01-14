72874c406973b11136c1b4b33f74e6e5daa63752
jongjunkim <54413901+jongjunkim@users.noreply.github.com>
2023-01-14 16:56:02 -0500

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+/*use sql.js in databse folder*/
+var sql = require('../database/sql') 
+
+const sectionIcons = [
+  '🍚', '🍿', '🍜', '🍣', '🥩', '☕', '🍰'
+]
+
+const statusKorMap = {
+  OPN: '영업중', 
+  CLS: '폐업',
+  VCT: '휴가중',
+  RMD: '리모델링'
+}
+
+router.get('/', async function(req, res, next) {
+
+  const sections = await sql.getSections()
+  sections.map((item) => {
+    item.icon = sectionIcons[item.section_id - 1]
+  })
+
+  res.render('sections', { 
+    title: 'Section list',
+    sections
+  });
+});
+
+
+router.get('/biz-simple', async function(req, res, next) {
+  const businesses = await sql.getBusinessesJoined(req.query)
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status]
+    return item
+  })
+
+  res.render('biz-simple', { 
+    title: '단순 식당 목록',
+    businesses
+  });
+});
+
+router.get('/biz-adv', async function(req, res, next) {
+  const businesses = await sql.getBusinessesJoined(req.query)
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status]
+    return item
+  })
+
+  res.render('biz-adv', { 
+    title: '고급 식당 목록',
+    q: req.query,
+    businesses
+  });
+}); 
+router.get('/business/:id', async function(req, res, next) {
+  const biz = await sql.getSingleBusinessJoined(req.params.id)
+  biz.status_kor = statusKorMap[biz.status]
+  biz.icon = sectionIcons[biz.section_id - 1]
+
+  const menus = await sql.getMenusOfBusiness(req.params.id)
+  const ratings = await sql.getRatingsOfBusiness(req.params.id)
+
+  res.render('detail', { 
+    biz,
+    menus,
+    ratings
+  });
+});
+
+router.put('/menus/:id', async function(req, res, next) {
+  const result = await sql.updateMenuLikes(req.params.id, req.body.like)
+  res.send(result)
+});
+module.exports = router;
